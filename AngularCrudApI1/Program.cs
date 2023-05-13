5a1889faea61b31ae31a4c16776690bac766b7a4
kamal <klpradhan89@gmail.com>
2023-05-13 08:19:44 +0530

    Just Checking

--- AngularCrudApI1/Program.cs
+++ AngularCrudApI1/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//just checking
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
